9e238460902727d1d233ae2884053ad60733cd2a
AdityaaSinghh07 <aditya.singh@codelogicx.com>
2026-05-26 10:29:30 +0000

    Initial project scaffold

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.end('<h1>Frontend is running</h1>');
+});
+server.listen(PORT, () => console.log(`Frontend running on port ${PORT}`));
